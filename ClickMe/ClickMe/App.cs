34282757b5c36085dd5a85d4c348bbb75510cd4e
charri <git@a.charri.ch>
2017-05-22 21:49:01 +0200

    first commit

---
+++ ClickMe/ClickMe/App.cs
@@ -0,0 +1,20 @@
+﻿using ClickMe.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ClickMe
+{
+    public class App : Application
+    {
+        public App()
+        {
+            
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
---
+++ ClickMe/ClickMe/Views/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace ClickMe.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private double _clickNumber = (int)Device.GetNamedSize(NamedSize.Small, typeof(Label));
+        public double ClickNumber
+        {
+            get { return _clickNumber; }
+
+            set
+            {
+                if (_clickNumber == value)
+                    return;
+
+                _clickNumber = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void ButtonClick(object sender, EventArgs arg)
+        {
+            ClickNumber *= 1.5;
+        }
+    }
+}
